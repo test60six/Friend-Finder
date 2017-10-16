b69504568bb743f78b21cf2505931a4212bced45
test60six <test60six@gmail.com>
2017-10-16 17:34:06 -0400

    update

---
+++ routing/server.js
@@ -0,0 +1,18 @@
+var path = require("path");
+var bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// app.use(express.static(path.join(__dirname, './public/home')));
+
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
